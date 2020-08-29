af8687e2007e600ac86eb1ebb7d5f8624a4166d4
Martin Kopeček <github@smallhill.cz>
2020-08-29 17:11:00 +0200

    Ad filter structure

--- src/app/pages/dataset-list/dataset-list.component.ts
+++ src/app/pages/dataset-list/dataset-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CatalogService } from 'app/services/catalog.service';
 import { Dataset } from 'app/schema';
 
+import { Theme } from "otevrene-formalni-normy-dts";
+
 @Component({
   selector: 'app-dataset-list',
   templateUrl: './dataset-list.component.html',
@@ -12,7 +14,11 @@ export class DatasetListComponent implements OnInit {
   datasets: Partial<Dataset>[] = [];
   count: number = 0;
 
-  constructor(private catalog: CatalogService) { }
+  themes: { title: string, iri: string }[] = Object.entries(Theme).map(([key, value]) => ({ title: key, iri: String(value) }));
+
+
+  constructor(private catalog: CatalogService) {
+  }
 
   ngOnInit(): void {
     this.loadDatasets();
