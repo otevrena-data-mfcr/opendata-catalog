7e858070bc3b184d5c0c921795b04d97c6eb390f
Martin Kopeček <github@smallhill.cz>
2020-12-08 16:30:00 +0100

    Implement Angular app as web component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { ConfigService } from './services/config.service';
 
 @Component({
   selector: 'opendata-catalog',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'opendata-catalog';
 
   constructor(
-    configService: ConfigService
+    private router: Router
   ) { }
+
+  ngOnInit() {
+    this.router.initialNavigation();
+  }
 }
