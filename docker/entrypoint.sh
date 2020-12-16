629c7365efaddaf64cb3eeb5f7798212eebcfc93
Martin Kopeček <github@smallhill.cz>
2020-12-16 20:10:34 +0100

    Fix docker string replacement

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 
-sed -i 's@%SPARQL_ENDPOINT%@'"$TEST"'@' index.html
+sed -i 's@%SPARQL_ENDPOINT%@'"$SPARQL_ENDPOINT"'@' /usr/share/nginx/html/index.html
+echo "Replaced %SPARQL_ENDPOINT% with $SPARQL_ENDPOINT"
 
-/docker-entrypoint.sh
+/docker-entrypoint.sh nginx -g "daemon off;"
