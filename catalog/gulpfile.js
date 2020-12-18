2bb6c94212ca7bdbc24ff6fb9e65675682c1addf
Martin Kopeček <github@smallhill.cz>
2020-12-18 13:03:21 +0100

    convert gulpfile to js from ts so tsconfig can target es2015

---
+++ catalog/gulpfile.js
@@ -0,0 +1,26 @@
+
+const { src, dest, series, parallel } = require("gulp");
+const concat = require("gulp-concat");
+
+const { rmdirSync, mkdirSync } = require("fs");
+
+function clearPackageDir(cb) {
+  rmdirSync("./package/", { recursive: true });
+  mkdirSync("./package/");
+  cb();
+}
+
+function buildPackage() {
+  return src(["./dist/*.js"])
+    .pipe(concat("catalog.js"))
+    .pipe(dest("./package/"));
+}
+
+function copyAssets() {
+  const assets = [
+    "./dist/3rdpartylicenses.txt"
+  ];
+  return src(assets).pipe(dest("./package/"));
+}
+
+exports.buildPackage = series(clearPackageDir, buildPackage, copyAssets);
